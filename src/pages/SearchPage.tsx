791a21d5a0c04f3e4804705b4eb85f0dcac817f4
Martin Růžek <me@martinruzek.cz>
2025-05-16 16:06:57 +0200

    added padding to the bottom of the search page

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -12,7 +12,7 @@ const SearchPage = () => {
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <div className="flex h-screen w-full flex-col bg-gray-800 shadow-2xl shadow-zinc-900 sm:w-3/5 md:w-3/5 lg:w-2/5 xl:w-1/4">
         <Header />
-        <main className="flex w-full flex-col overflow-y-scroll">
+        <main className="flex w-full flex-col overflow-y-scroll pb-5">
           <SearchBar />
 
           <div>
