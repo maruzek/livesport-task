338a4d92aec42a215b7aa5529de43e57cb950587
Martin Růžek <me@martinruzek.cz>
2025-05-17 13:38:11 +0200

    created basic favorites page

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -7,7 +7,7 @@ type BasicLayoutProps = {
 const BasicLayout = ({ children }: BasicLayoutProps) => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <div className="flex h-screen w-full flex-col bg-gray-800 shadow-2xl shadow-zinc-900 sm:w-3/5 md:w-3/5 lg:w-2/5 xl:w-1/4">
+      <div className="relative flex h-screen w-full flex-col bg-gray-800 shadow-2xl shadow-zinc-900 sm:w-3/5 md:w-3/5 lg:w-2/5 xl:w-1/4">
         <Header />
         <main className="flex w-full flex-col overflow-y-scroll pb-5">
           {children}
--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,5 +1,32 @@
+import BasicLayout from "../layouts/BasicLayout";
+import { useFavorites } from "../hooks/useFavorites";
+import SearchItem from "../components/SearchItem";
+import BottomNav from "../components/BottomNav";
+import { Link } from "react-router";
+
 const FavouritesPage = () => {
-  return <div>FavouritesPage</div>;
+  const { favorites } = useFavorites();
+
+  return (
+    <BasicLayout>
+      <section className="p-4">
+        <h2 className="text-2xl font-bold">Your favourites</h2>
+      </section>
+      <section className="">
+        {favorites.map((item) => (
+          <Link
+            key={item.id}
+            to={`/detail`}
+            state={{ item }}
+            className="block transition-colors duration-100 hover:bg-gray-700"
+          >
+            <SearchItem item={item} key={item.id} />
+          </Link>
+        ))}
+      </section>
+      <BottomNav />
+    </BasicLayout>
+  );
 };
 
 export default FavouritesPage;
