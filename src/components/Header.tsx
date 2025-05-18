7ba73a669407c16727e03dd6a40fdf8b6f8a723e
Martin Růžek <me@martinruzek.cz>
2025-05-18 09:34:04 +0200

    layout final fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
   };
 
   return (
-    <header className="flex h-16 w-full items-center justify-between bg-gray-700 p-4">
+    <header className="flex h-16 w-full shrink-0 items-center justify-between bg-gray-700 p-4">
       <div className="flex items-center gap-3">
         <button
           className="cursor-pointer transition-colors duration-100 hover:text-gray-400"
--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -8,8 +8,8 @@ type BasicLayoutProps = {
 
 const BasicLayout = ({ children, displayBottomNav }: BasicLayoutProps) => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <div className="relative flex h-screen w-full flex-col bg-gray-800 shadow-2xl shadow-zinc-900 sm:w-3/5 md:w-3/5 lg:w-2/5 xl:w-1/4">
+    <div className="flex h-[100dvh] w-screen flex-col items-center justify-center">
+      <div className="relative flex h-full w-full flex-col bg-gray-800 shadow-2xl shadow-zinc-900 sm:w-3/5 md:w-3/5 lg:w-2/5 xl:w-1/4">
         <Header />
         <main className="flex grow flex-col overflow-y-auto">{children}</main>
         {displayBottomNav && <BottomNav />}
