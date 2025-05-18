adfa3ae3f45e28fbdb57117bc824e1941336a82f
Martin Růžek <me@martinruzek.cz>
2025-05-18 21:43:55 +0200

    created tests for SearchItem and SearchError

--- src/components/SearchItem.tsx
+++ src/components/SearchItem.tsx
@@ -20,9 +20,15 @@ const SearchItem = ({
     ? `${import.meta.env.VITE_IMG_URL}${item.images.filter((img) => img.variantTypeId == 15)[0]?.path}`
     : placeholder;
 
-  const imgDefaultCountry =
-    `${import.meta.env.VITE_IMG_URL}${item.defaultCountry.images.filter((img) => img.variantTypeId == 87)[0]?.path}` ||
-    "";
+  let imgDefaultCountry = "";
+
+  if (
+    item.defaultCountry.images.filter((img) => img.variantTypeId == 87)[0]?.path
+  ) {
+    imgDefaultCountry =
+      `${import.meta.env.VITE_IMG_URL}${item.defaultCountry.images.filter((img) => img.variantTypeId == 87)[0]?.path}` ||
+      "";
+  }
 
   const team = item.teams?.filter(
     (team) => team.participantType.name == "Team",
@@ -48,11 +54,13 @@ const SearchItem = ({
         <div>
           <h4 className="text-md flex items-center gap-2 font-bold">
             {item.name}
-            <img
-              src={imgDefaultCountry}
-              alt={`Flag of ${item.defaultCountry.name}`}
-              className="h-4 w-4"
-            />
+            {imgDefaultCountry && (
+              <img
+                src={imgDefaultCountry}
+                alt={`Flag of ${item.defaultCountry.name}`}
+                className="h-4 w-4 rounded-full"
+              />
+            )}
           </h4>
           <span className="text-sm text-gray-400">{team?.name}</span>
         </div>
